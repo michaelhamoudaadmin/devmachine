dd93772a2bd0693f7a355b4114b92e4182b6699b
root <root@devmachine.localdomain>
2019-10-08 21:05:54 +0000

    aws cli

--- install.sh
+++ install.sh
@@ -72,21 +72,25 @@ unzip terraform_0.12.9_linux_amd64.zip
 echo "alias tf='terraform'" >> ~/.bash_profile
 echo "alias tfa='tf apply -auto-approve'" >> ~/.bash_profile
 echo "alias tfd='tf destroy -f'" >> ~/.bash_profile
-
 echo "alias wip=\"git add . && git commit -m \"wip\" && git push\"" >> ~/.bash_profile
 echo "alias gs='git status'" >> ~/.bash_profile
 echo "alias gaa='git add .'" >> ~/.bash_profile
 echo "alias gau='git add -u'" >> ~/.bash_profile
 echo "alias gc='git commit -m'" >> ~/.bash_profile
 echo "alias k='kubectl'" >> ~/.bash_profile
-
 echo "alias d='docker'" >> ~/.bash_profile
 echo "alias dc='docker-compose'" >> ~/.bash_profile
 echo "alias r='ruby'" >> ~/.bash_profile
-
 echo "alias sbp='source ~/.bash_profile'" >> ~/.bash_profile
 echo "alias ebp='vim ~/.bash_profile'" >> ~/.bash_profile
 echo "alias cbp='cat ~/.bash_profile'" >> ~/.bash_profile
 echo "alias editor=vim" >> ~/.bash_profile
-
 source ~/.bash_profile
+
+#install aws cli
+cd /tmp && \
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip" && \
+unzip awscli-bundle.zip && \
+cd awscli-bundle/ && \
+./install -i /usr/local/aws -b /usr/local/bin/aws
+cd
