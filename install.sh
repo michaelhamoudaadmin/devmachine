d82fffa31721832ea8136f529d145dc2e781b342
root <root@devmachine.localdomain>
2019-10-08 20:56:02 +0000

    wip

--- install.sh
+++ install.sh
@@ -68,3 +68,25 @@ mv terraform_0.12.9_linux_amd64.zip /usr/local/bin && \
 cd /usr/local/bin && \
 unzip terraform_0.12.9_linux_amd64.zip
 
+#linux aliases
+echo "alias tf='terraform'" >> ~/.bash_profile
+echo "alias tfa='tf apply -auto-approve'" >> ~/.bash_profile
+echo "alias tfd='tf destroy -f'" >> ~/.bash_profile
+
+echo "alias wip=\"git add . && git commit -m \"wip\" && git push\"" >> ~/.bash_profile
+echo "alias gs='git status'" >> ~/.bash_profile
+echo "alias gaa='git add .'" >> ~/.bash_profile
+echo "alias gau='git add -u'" >> ~/.bash_profile
+echo "alias gc='git commit -m'" >> ~/.bash_profile
+echo "alias k='kubectl'" >> ~/.bash_profile
+
+echo "alias d='docker'" >> ~/.bash_profile
+echo "alias dc='docker-compose'" >> ~/.bash_profile
+echo "alias r='ruby'" >> ~/.bash_profile
+
+echo "alias sbp='source ~/.bash_profile'" >> ~/.bash_profile
+echo "alias ebp='vim ~/.bash_profile'" >> ~/.bash_profile
+echo "alias cbp='cat ~/.bash_profile'" >> ~/.bash_profile
+echo "alias editor=vim" >> ~/.bash_profile
+
+source ~/.bash_profile
